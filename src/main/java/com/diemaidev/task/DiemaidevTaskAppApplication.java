a82ebd3c90d312307847431b7e34452ba3dfba20
Diego Maidana <die.maidana1@gmail.com>
2026-02-06 16:52:06 -0300

    feat: inital project setup with Spring Boot and Docker

---
+++ src/main/java/com/diemaidev/task/DiemaidevTaskAppApplication.java
@@ -0,0 +1,13 @@
+package com.diemaidev.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiemaidevTaskAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiemaidevTaskAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diemaidev/task/DiemaidevTaskAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diemaidev.task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiemaidevTaskAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
